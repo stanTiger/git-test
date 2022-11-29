a0d2a72e661685c44a864c44c2c3030f8b1286e0
stan <stantiger2022@gmail.com>
2022-11-29 17:46:40 +0800

    main test

--- src/main/java/com/stan/git/HelloGit.java
+++ src/main/java/com/stan/git/HelloGit.java
@@ -7,5 +7,6 @@ public class HelloGit {
         System.out.println("Hello git2");
         System.out.println("Hello git3");
         System.out.println("hot fix update");
+        System.out.println("main test");
     }
 }
