7ae8ab8dba68b4d25730352363198324a6009c6b
stan <stantiger2022@gmail.com>
2022-11-29 17:37:26 +0800

    idea git third commit

--- src/main/java/com/stan/git/HelloGit.java
+++ src/main/java/com/stan/git/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
